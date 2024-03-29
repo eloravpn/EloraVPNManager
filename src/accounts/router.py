a3a4520f47a459fa5eb639def5e67b58962502db
Elora VPN <eloravpn@gmail.com>
2024-03-29 23:24:16 +0330

    add API route for /accounts/bulk_extend

--- src/accounts/router.py
+++ src/accounts/router.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
+import math
 from typing import List
 
+import humanize
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.openapi.models import Response
 from pyasn1.type.univ import Integer
@@ -10,6 +12,7 @@ from sqlalchemy.orm import Session
 
 import src.accounts.service as service
 import src.users.service as user_service
+from src import config, messages
 from src.accounts.schemas import (
     AccountCreate,
     AccountResponse,
@@ -23,10 +26,104 @@ from src.accounts.schemas import (
 from src.admins.schemas import Admin
 from src.database import get_db
 from src.hosts.service import get_host_zone
+from src.notification.schemas import NotificationCreate, NotificationType
+from src.notification.service import create_notification
 
 router = APIRouter()
 
 logger = logging.getLogger("uvicorn.error")
+
+
+@router.put("/accounts/bulk_extend", tags=["Account"])
+def modify_account(
+    db: Session = Depends(get_db),
+    admin: Admin = Depends(Admin.get_current),
+    pre_message_text: str = "",
+    extend_day: int = 0,
+    extend_data_limit_percent: int = 0,
+):
+
+    accounts, count = service.get_accounts(
+        filter_enable=True, db=db, enable=True, test_account=False
+    )
+
+    total_extend_gb = 0
+
+    for db_account in accounts:
+        data_limit = db_account.data_limit
+        used_traffic = db_account.used_traffic
+
+        expire_date = db_account.expired_at
+
+        if expire_date:
+            extend_expire_date = expire_date + datetime.timedelta(days=extend_day)
+
+        extend_data_limit_gb = 0
+        extended_data_limit = 0
+
+        if data_limit > 0 and extend_data_limit_percent > 0:
+            extend_data_limit_gb = math.ceil(
+                ((extend_data_limit_percent / 100) * (data_limit - used_traffic))
+                / pow(1024, 3)
+            )
+            extended_data_limit = data_limit + (extend_data_limit_gb * pow(1024, 3))
+
+            total_extend_gb += extend_data_limit_gb
+
+            data_limit_readable = humanize.naturalsize(
+                data_limit, binary=True, gnu=True, format="%.0f"
+            )
+            new_data_limit_readable = humanize.naturalsize(
+                extended_data_limit, binary=True, gnu=True, format="%.0f"
+            )
+            logger.info(
+                f"Service Traffic with id {db_account.email} owned by user:{db_account.user.full_name} "
+                f"Extend from {data_limit_readable} To {new_data_limit_readable}"
+            )
+            logger.info(
+                f"Service Expire Date with id {db_account.email} owned by user:{db_account.user.full_name} "
+                f"Extend from {expire_date} To {extend_expire_date}"
+            )
+
+        account_modify = AccountModify(
+            id=db_account.id,
+            user_id=db_account.user_id,
+            host_zone_id=db_account.host_zone_id,
+            uuid=db_account.uuid,
+            data_limit=extended_data_limit,
+            used_traffic=db_account.used_traffic,
+            ip_limit=db_account.ip_limit,
+            email=db_account.email,
+            enable=True,
+            expired_at=extend_expire_date,
+        )
+
+        service.update_account(
+            db=db,
+            db_account=db_account,
+            modify=account_modify,
+            db_host_zone=db_account.host_zone,
+        )
+
+        message = pre_message_text + messages.USER_NOTIFICATION_ACCOUNT_EXTENDED.format(
+            id=db_account.email,
+            extend_day=extend_day,
+            extend_data_limit_gb=extend_data_limit_gb,
+            admin_id=config.TELEGRAM_ADMIN_USER_NAME,
+        )
+
+        create_notification(
+            db=db,
+            db_user=db_account.user,
+            notification=NotificationCreate(
+                approve=True,
+                message=message,
+                level=0,
+                type=NotificationType.account,
+            ),
+        )
+
+    return {"total": count, "total_extend_gb": total_extend_gb}
 
 
 @router.post(
